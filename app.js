beab7996a0639f519d3fe4f18d1a30304affa279
Alexandre Lima <alexandreatlima@gmail.com>
2022-04-25 16:40:15 -0300

    bugfix

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const data = require("./data");
 const axios = require("axios");
 
-const API_COLLECTION_NAME = "coloque o nome da sua coleção aqui";
+const API_COLLECTION_NAME = "teste-fill";
+
+async function postAPI(item) {
+  await axios.post(
+    `https://ironrest.herokuapp.com/${API_COLLECTION_NAME}`,
+    item
+  );
+}
 
 for (let i = 0; i < data.length; i++) {
-  axios.post(`https://ironrest.heroku.com/${API_COLLECTION_NAME}`, data[i]);
+  postAPI(data[i]);
 }
